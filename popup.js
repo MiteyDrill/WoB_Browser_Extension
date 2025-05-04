294a355fd5e5c9753d7830f88f4be13bed345377
Ben <theben.business@gmail.com>
2025-05-04 12:35:09 -0700

    feat: add remove button to delete users from list

--- popup.js
+++ popup.js
@@ -1,17 +1,64 @@
 const form = document.getElementById('avatarForm');
 const list = document.getElementById('avatarList');
 
+function removeItemFromStorageAndUI(id, btn) {
+  chrome.storage.local.get("avatarMap", (data) => {
+    const items = data.avatarMap || {};
+    delete items[id]
+    console.log(`${id} has been removed.`);
+
+    chrome.storage.local.set({ avatarMap: items }, () => {
+      // DOM update after save
+      const li = btn.closest("li");
+      if (li) li.remove();
+    });
+
+    //re-render list
+    chrome.storage.local.get('avatarMap', data => {
+      renderList(data.avatarMap || {});
+      addDeleteButtonFunctionality();
+    });
+
+    //TODO: Refresh Page
+  });
+}
+
+function addDeleteButtonFunctionality(){
+  const buttons = list.querySelectorAll("a");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = btn.dataset.id = btn.id;
+      removeItemFromStorageAndUI(id, btn);
+    });
+  });
+}
+
 function renderList(map) {
   list.innerHTML = '';
   for (const user in map) {
+
+    // List Item Element
     const li = document.createElement('li');
     li.textContent = `${user}: ${map[user]}`;
+    li.id = user;
     list.appendChild(li);
+
+    // Button Icon Element
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent = "remove";
+    deleteButton.style.color = "red";
+    deleteButton.href = '#';    
+    deleteButton.id = user; //associated User List Item
+
+    list.appendChild(deleteButton);
   }
 }
 
 chrome.storage.local.get('avatarMap', data => {
   renderList(data.avatarMap || {});
+  addDeleteButtonFunctionality();
 });
 
 form.addEventListener('submit', e => {
